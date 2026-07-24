d8c2a064029fb2e5ebec76e972f5890f550f67e1
Alfonso García <agcortijo@gmail.com>
2026-07-24 11:30:33 +0200

    chore(): update pyproject.toml

--- models/sentiment/main.py
+++ models/sentiment/main.py
@@ -1,25 +1,25 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from peft import PeftModel
-
 BASE_MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 LORA_MODEL = "Bitnick42/roberta-base-review-sentiment-analysis"
 LABEL2ID = {"negative": 0, "neutral": 1, "positive": 2}
 ID2LABEL = {v: k for k, v in LABEL2ID.items()}
 
-tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
-model = AutoModelForSequenceClassification.from_pretrained(BASE_MODEL)
-model = PeftModel.from_pretrained(
-    model,
-    LORA_MODEL
-)
-
-model.eval()
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
-
 def sentiment_analysis(review):
+    import torch
+    from transformers import AutoTokenizer, AutoModelForSequenceClassification
+    from peft import PeftModel
+    
+    tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
+    model = AutoModelForSequenceClassification.from_pretrained(BASE_MODEL)
+    model = PeftModel.from_pretrained(
+        model,
+        LORA_MODEL
+    )
+
+    model.eval()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
     review_title = review['title']
     review_content = review['content']
     review_input = ' '.join([review_title, review_content])
