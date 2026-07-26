3d629ffef941874a87d4bc2cea7c00c383c6bf96
Alfonso García <agcortijo@gmail.com>
2026-07-26 10:51:05 +0200

    feat(): add pagination on products reviews list

--- api/products.py
+++ api/products.py
@@ -25,9 +25,10 @@ async def api_get_products_list():
 async def api_get_product(product_id):
     return await get_product(product_id)
 
-@router.get('/{product_id}/reviews')
+@router.get('/{product_id}/reviews', response_model=Page[dict])
 async def api_get_products_reviews_list(product_id):
-    return await get_products_reviews_list(product_id)
+    products_reviews_list = await get_products_reviews_list(product_id)
+    return paginate(products_reviews_list)
 
 @router.get('/{product_id}/reviews/summarize')
 async def api_summarize_product_reviews(product_id):
